814d6a5e3fd761d28b60f875527048ce0bc884b2
Matthieu Borgognon <matthieu.borgognon@advance-ticket.ch>
2023-05-07 19:51:09 +0200

    Add capability for local routing on petal link

--- web/src/components/NavigationLogo/NavigationLogo.tsx
+++ web/src/components/NavigationLogo/NavigationLogo.tsx
@@ -1,8 +1,16 @@
 import { Button } from '@chakra-ui/react'
 
-import { FormError } from '@redwoodjs/forms'
+import { navigate, routes } from '@redwoodjs/router'
 
 import { LogoTM4Soul } from '../Logo/Logo'
+
+type PetalLink = {
+  link: string
+}
+
+type Petal = {
+  [key: string]: PetalLink
+}
 
 class NavigationLogo extends React.Component {
   constructor(props) {
@@ -11,8 +19,8 @@ class NavigationLogo extends React.Component {
   }
   componentDidMount() {
     console.log('NavigationLogo mounted')
-    const petals = {
-      'petal_right-down': { link: 'https://www.tm4soul.ch/' },
+    const petals: Petal = {
+      'petal_right-down': { link: routes.photography() },
       'petal_left-down': { link: 'https://www.tm4soul.ch/' },
       'petal_right-up': { link: 'https://www.tm4soul.ch/' },
       'petal_left-up': { link: 'https://www.tm4soul.ch/' },
@@ -21,53 +29,26 @@ class NavigationLogo extends React.Component {
     // for loop for object petals
     for (const [key, value] of Object.entries(petals)) {
       const el = document.getElementById(key)
-      // el.addEventListener('mouseover', this.handleButtonClick.bind(this))
+
       el.addEventListener('mouseover', () => (el.style.fill = 'white'))
       el.addEventListener('mouseleave', () => (el.style.fill = 'red'))
-      // TODO: Add link to route point photography
-      el.addEventListener('click', () => (window.location.href = value.link))
-    }
-  }
 
-  handleButtonClick() {
-    console.log('clicked')
-    const el = document.getElementById('petal_right-down')
-    // toggle fill color from white to black with a ternary operator
-    el.style.fill = el.style.fill === 'white' ? 'red' : 'white'
+      // if value.link starts with '/' navigate to that local page
+      el.addEventListener('click', () => {
+        if (value.link.charAt(0) === '/') {
+          navigate(value.link)
+        } else {
+          window.location.href = value.link
+        }
+      })
+    }
   }
 
   render() {
     return (
       <>
         <LogoTM4Soul />
-        <Button onClick={this.handleButtonClick.bind(this)}>Click me</Button>
       </>
-      // <div
-      //   style={{
-      //     position: 'relative',
-      //   }}
-      // >
-      //   <div
-      //     style={{
-      //       width: '2500px',
-      //       position: 'absolute',
-      //       left: '50%',
-      //       top: '0px',
-      //       marginLeft: '-1250px',
-      //       zIndex: '1',
-      //     }}
-      //   >
-      //     <img
-      //       src="https://tm4soul.ch/assets/img/logo_tm4soul.jpg"
-      //       alt=""
-      //       style={{
-      //         display: 'block',
-      //         margin: '0 auto',
-      //         clipPath: 'inset(0 500px 10px 0)',
-      //       }}
-      //     />
-      //   </div>
-      // </div>
     )
   }
 }
