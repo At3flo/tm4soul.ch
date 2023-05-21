1759fabd54ec0bfc1987c84d8e4e3adaad24283a
Matthieu Borgognon <mat.gh@bqn.ch>
2023-05-21 11:31:01 +0200

    Change pointer if active link

--- web/src/components/NavigationLogo/NavigationLogo.tsx
+++ web/src/components/NavigationLogo/NavigationLogo.tsx
@@ -17,27 +17,35 @@ class NavigationLogo extends React.Component {
   }
   componentDidMount() {
     const petals: Petal = {
-      'petal_right-down': { link: routes.photography() },
-      'petal_left-down': { link: 'https://www.tm4soul.ch/' },
-      'petal_right-up': { link: 'https://www.tm4soul.ch/' },
-      'petal_left-up': { link: 'https://www.tm4soul.ch/' },
+      'petal_right-down': { link: null },
+      'petal_left-down': { link: null },
+      'petal_right-up': { link: null },
+      'petal_left-up': { link: routes.photography() },
     }
 
     // loop on every petal SVG part to create hover effect
     for (const [key, value] of Object.entries(petals)) {
       const el = document.getElementById(key)
 
-      el.addEventListener('mouseover', () => (el.style.fill = 'yellow'))
-      el.addEventListener('mouseleave', () => (el.style.fill = 'white'))
+      if (value.link === null) {
+        el.addEventListener('mouseover', () => (el.style.fill = 'gray'))
+        el.addEventListener('mouseleave', () => (el.style.fill = 'white'))
+      } else {
+        el.addEventListener('mouseover', () => {
+          el.style.fill = 'yellow'
+          el.style.cursor = 'pointer'
+        })
+        el.addEventListener('mouseleave', () => (el.style.fill = 'white'))
 
-      // if value.link starts with '/' navigate to that local page
-      el.addEventListener('click', () => {
-        if (value.link.charAt(0) === '/') {
-          navigate(value.link)
-        } else {
-          window.location.href = value.link
-        }
-      })
+        // if value.link starts with '/' navigate to that local page
+        el.addEventListener('click', () => {
+          if (value.link.charAt(0) === '/') {
+            navigate(value.link)
+          } else {
+            window.location.href = value.link
+          }
+        })
+      }
     }
   }
 
