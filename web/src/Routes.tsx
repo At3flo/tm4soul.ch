a3d38a3565bc7802f3697e92f7544c5a48996409
Matthieu Borgognon <matthieu.borgognon@advance-ticket.ch>
2024-04-21 12:28:43 +0200

    chore: move tags and images behind protected zone

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { PrivateSet, Set, Router, Route } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
@@ -20,18 +20,20 @@ const Routes = () => {
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Set wrap={ScaffoldLayout} title="Images" titleTo="images" buttonLabel="New Image" buttonTo="newImage">
-        <Route path="/images/new" page={ImageNewImagePage} name="newImage" />
-        <Route path="/images/{uuidImage}/edit" page={ImageEditImagePage} name="editImage" />
-        <Route path="/images/{uuidImage}" page={ImageImagePage} name="image" />
-        <Route path="/images" page={ImageImagesPage} name="images" />
-      </Set>
-      <Set wrap={ScaffoldLayout} title="Tags" titleTo="tags" buttonLabel="New Tag" buttonTo="newTag">
-        <Route path="/tags/new" page={TagNewTagPage} name="newTag" />
-        <Route path="/tags/{uuidTag}/edit" page={TagEditTagPage} name="editTag" />
-        <Route path="/tags/{uuidTag}" page={TagTagPage} name="tag" />
-        <Route path="/tags" page={TagTagsPage} name="tags" />
-      </Set>
+      <PrivateSet unauthenticated="home">
+        <Set wrap={ScaffoldLayout} title="Images" titleTo="images" buttonLabel="New Image" buttonTo="newImage">
+          <Route path="/admin/images/new" page={ImageNewImagePage} name="newImage" />
+          <Route path="/admin/images/{uuidImage}/edit" page={ImageEditImagePage} name="editImage" />
+          <Route path="/admin/images/{uuidImage}" page={ImageImagePage} name="image" />
+          <Route path="/admin/images" page={ImageImagesPage} name="images" />
+        </Set>
+        <Set wrap={ScaffoldLayout} title="Tags" titleTo="tags" buttonLabel="New Tag" buttonTo="newTag">
+          <Route path="/admin/tags/new" page={TagNewTagPage} name="newTag" />
+          <Route path="/admin/tags/{uuidTag}/edit" page={TagEditTagPage} name="editTag" />
+          <Route path="/admin/tags/{uuidTag}" page={TagTagPage} name="tag" />
+          <Route path="/admin/tags" page={TagTagsPage} name="tags" />
+        </Set>
+      </PrivateSet>
       <Route path="/gallery" page={GalleryPage} name="gallery" />
       <Route path="/photography" page={PhotographyPage} name="photography" />
       <Route path="/" page={HomePage} name="home" />
